b118637ab5607bc81b39715d05af43f5df4f4d3a
GaneshPandey-GP <ganeshpandey897@gmail.com>
2021-01-05 20:36:34 +0530

    about page

---
+++ src/components/AboutData.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+const AboutData = () => {
+  return (
+    <div class="md:flex max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-5xl mt-16 bg-gray-200 text-center">
+      <div class="p-8  w-full md:w-full lg:w-full text-center sm:text-left mb-5">
+        <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold mb-5 text-center">
+          A LITTLE ABOUT US
+          <hr />
+          SIMPLE THINGS SHOULD BE SIMPLE, COMPLEX THINGS SHOULD BE POSSIBLE.
+        </div>
+
+        <p>
+          We are infigini IT Services, providing multiple IT Services in various
+          fields. We work together to design, create, build, develop products
+          that we are proud of, for the people who believe in achieving their
+          dreams. We provide IT Services according to your requirement, weather
+          it be Mobile app development or Web App development, from Games to
+          full fledged SAAP Software, various types of Server Development to
+          Search Engine Optimization. The services provided by us are handled by
+          top professionals, whose work will satisfy all your requirements.
+        </p>
+      </div>
+    </div>
+  )
+}
+export default AboutData
