a63e20c0a32eeaf48a9d26babd956846ae62082e
GaneshPandey-GP <ganeshpandey897@gmail.com>
2021-01-20 10:26:59 +0530

    transparent

--- src/components/ShareContextProvider.js
+++ src/components/ShareContextProvider.js
@@ -4,7 +4,8 @@ const ShareContext = createContext()
 const ShareContextProvider = props => {
   const [collapsedShare, setCollapsedShare] = useState(props.theme)
   setInterval(() => {
-    let getMode  = JSON.parse(window.localStorage.getItem("mode"))
+    // let getMode  = JSON.parse(typeof window.localStorage.getItem("mode"))
+    let getMode = typeof window !== 'undefined' ? JSON.parse(localStorage.getItem("mode")) : ""
     setCollapsedShare(getMode)
   },1000/2)
 
