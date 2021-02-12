6be42326e8b5176ea5f0bff1927c496894302ff8
Tekraj Joshi <tekrajjoshi07@gmail.com>
2021-02-12 17:22:35 +0530

    build

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,13 +25,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Wideway Engineering Solutions`,
+        short_name: `WES`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/one.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-transformer-json`,
