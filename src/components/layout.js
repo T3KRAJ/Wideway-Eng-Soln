beaf8972c68438fcd62af49a8b1b15b5ae59bb04
Tekraj Joshi <tekrajjoshi07@gmail.com>
2021-02-20 23:34:27 +0530

    tk

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react'
-import Header from './Header'
+import Header from './header'
 import Footer from './Footer'
 import Sidebar from './Sidebar'
 import { GlobalStyle } from './styles/GlobalStyle'
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import About from "../components/About"
 import Contact from "../components/Contact"
 import Hero from "../components/Hero"
-import Gallery from '../components/Gallery'
+// import Gallery from '../components/Gallery'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Services from "../components/Services"
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <Hero />
     <About />
     <Services />
-    <Gallery />
+    {/* <Gallery /> */}
     <Contact />
   </Layout>
 )
