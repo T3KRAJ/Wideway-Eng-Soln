bbc2f82dd472e3361e2b9e0cd90978e95ab44d58
Tekraj Joshi <tekrajjoshi07@gmail.com>
2021-02-20 22:42:55 +0530

    tk

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Tekraj\\Documents\\reactprojects\\Gatsby_Projects\\Wideway-Eng-Soln\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Tekraj\\Documents\\reactprojects\\Gatsby_Projects\\Wideway-Eng-Soln\\src\\pages\\404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Tekraj\\Documents\\reactprojects\\Gatsby_Projects\\Wideway-Eng-Soln\\src\\pages\\index.js"))),
   "component---src-pages-using-typescript-tsx": hot(preferDefault(require("C:\\Users\\Tekraj\\Documents\\reactprojects\\Gatsby_Projects\\Wideway-Eng-Soln\\src\\pages\\using-typescript.tsx")))
