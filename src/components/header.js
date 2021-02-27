139a230dd3002c4e56fe356fcb09172fad6152ff
GaneshPandey-GP <ganeshpandey897@gmail.com>
2021-02-27 14:47:34 +0530

    g

--- src/components/header.js
+++ src/components/header.js
@@ -33,10 +33,10 @@ const Header = props => {
   return (
     <>
     <div className="icon-bar">
-  <a   rel=" noreferrer" href="https://www.facebook.com/Widewaysolutionscom-107382601397950/" target="_blank" className="facebook"><FontAwesomeIcon icon={faFacebook} size="lg"  role="button" aria-label="Facebook"/></a> 
-  <a  rel=" noreferrer" href="https://twitter.com/WidewayEngenie1/" target="_blank" className="twitter"><FontAwesomeIcon icon={faTwitter} size="lg" role="button" aria-label="Twitter"/></a> 
-  <a  rel=" noreferrer" href="#/" className="whatsapp"><FontAwesomeIcon icon={faWhatsapp} size="lg" role="button" aria-label="Whatsapp"/></a>
-  <a  rel=" noreferrer" href="https://www.instagram.com/wideway_engineering_solutions/" target="_blank" className="instagram"><FontAwesomeIcon icon={faInstagram} size="lg" role="button" aria-label="Instagram"/></a> 
+  <a   rel=" noreferrer" href="https://www.facebook.com/Widewaysolutionscom-107382601397950/" target="_blank" className="facebook"><FontAwesomeIcon icon={faFacebook} size="sm"  role="button" aria-label="Facebook"/></a> 
+  <a  rel=" noreferrer" href="https://twitter.com/WidewayEngenie1/" target="_blank" className="twitter"><FontAwesomeIcon icon={faTwitter} size="sm" role="button" aria-label="Twitter"/></a> 
+  <a  rel=" noreferrer" href="#/" className="whatsapp"><FontAwesomeIcon icon={faWhatsapp} size="sm" role="button" aria-label="Whatsapp"/></a>
+  <a  rel=" noreferrer" href="https://www.instagram.com/wideway_engineering_solutions/" target="_blank" className="instagram"><FontAwesomeIcon icon={faInstagram} size="sm" role="button" aria-label="Instagram"/></a> 
 
 </div>
       <nav ref={listener}>
@@ -57,7 +57,7 @@ const Header = props => {
                theme={props.theme !== null ? props.theme.toString(): null}
                id="navLink"
                 to={link}
-                activeClass="active"
+                // activeClass="active"
                 spy={true}
                 smooth={true}
                 offset={-70}
